f55243cfdfcaed49eeeeed73d26a7428912527b1
GeekSloth <84038443+geeksloth@users.noreply.github.com>
2022-10-20 17:27:22 +0700

    first commit

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import numpy as np
 import gradio as gr
-from time import sleep
 from rembg import remove
 
 
 def remove_background(input_img):
+	print(type(input_img))
 	output = remove(input_img)
 	return output
 
-demo = gr.Interface(remove_background, gr.Image(shape=(200, 200)), "image")
+demo = gr.Interface(remove_background, gr.Image(), "image")
 demo.launch(server_name="0.0.0.0")
